6011039d16e555cea567d7dc549a6524ebb82e34
kchelvan <keshopan.chelvan@gmail.com>
2025-03-02 16:51:01 -0500

    Added Error logging for fetchFilms

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -6,7 +6,13 @@ export interface UseFetchFilmResponse {
 
 export const fetchFilms = async ({ route }: { route: string }) => {
 	const res = await fetch(`${process.env.API_URL}${route}`);
+	console.error('debug error: res', res, `${process.env.API_URL}${route}`);
 	const respData = await res.json();
 
+	console.error(
+		'debug error: respData',
+		respData,
+		`${process.env.API_URL}${route}`
+	);
 	return { films: respData?.data || [] };
 };
