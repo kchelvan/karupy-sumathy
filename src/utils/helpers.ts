5c988ee9dfd646493f87504b8bb920f99da1586f
kchelvan <keshopan.chelvan@gmail.com>
2025-03-02 16:53:56 -0500

    Added Error logging for fetchFilms

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -6,10 +6,11 @@ export interface UseFetchFilmResponse {
 
 export const fetchFilms = async ({ route }: { route: string }) => {
 	const res = await fetch(`${process.env.API_URL}${route}`);
-	console.error('debug error: res', res, `${process.env.API_URL}${route}`);
+	// eslint-disable-next-line no-console
+	console.log('debug error: res', res, `${process.env.API_URL}${route}`);
 	const respData = await res.json();
-
-	console.error(
+	// eslint-disable-next-line no-console
+	console.log(
 		'debug error: respData',
 		respData,
 		`${process.env.API_URL}${route}`
