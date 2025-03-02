db609dfb9a75e277f4973641057c985a26572fae
kchelvan <keshopan.chelvan@gmail.com>
2025-03-02 16:46:22 -0500

    Updated API_URL in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -7,7 +7,9 @@ const nextConfig: NextConfig = {
 		BASE_URL: process.env.BASE_URL,
 		BASE_ENV: process.env.BASE_ENV,
 		API_URL: `${process.env.BASE_ENV != 'development' ? 'https' : 'http'}://${
-			process.env.BASE_URL
+			process.env.BASE_ENV != 'development'
+				? process.env.VERCEL_URL
+				: 'localhost:3001'
 		}/api/`,
 	},
 };
