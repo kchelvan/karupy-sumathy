211be33610addc0d4d312133d1553b196f89ca4e
kchelvan <keshopan.chelvan@gmail.com>
2025-03-02 04:34:06 -0500

    Feature: Removed Unused API call in Feature Films

--- src/app/feature-films/page.tsx
+++ src/app/feature-films/page.tsx
@@ -1,10 +1,6 @@
 import React from 'react';
 
 const FeatureFilmListPage = async () => {
-	await fetch(`${process.env.BASE_URL}/api/feature-film`).then((res) =>
-		res.json()
-	);
-
 	return <div>FeatureFilmListPage</div>;
 };
 
