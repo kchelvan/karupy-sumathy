588d2a47977c18a74ba5297c5099bdc2bfdd92c5
kchelvan <keshopan.chelvan@gmail.com>
2025-03-02 02:42:56 -0500

    Feature: Updated a tag to Next Link for Home Nav Button

--- src/components/layout/header/components/home-button/index.tsx
+++ src/components/layout/header/components/home-button/index.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import styles from './home-button.module.scss';
+import Link from 'next/link';
 
 const HomeButton = () => {
 	return (
-		<a className={styles.homeButton} href='/'>
+		<Link className={styles.homeButton} href='/'>
 			<p className={styles.homeButtonText}>Karupy Sumathy</p>
 			<p className={styles.homeButtonSubText}>Filmmaker, Storyteller</p>
-		</a>
+		</Link>
 	);
 };
 
